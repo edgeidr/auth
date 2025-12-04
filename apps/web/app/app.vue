4cfea300bb8f8588f78840517793021f7617dfa9
edgeidr <edge.idr@gmail.com>
2025-12-05 03:47:36 +0800

    feat(web): add new layout - auth

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -1,6 +1,7 @@
 <template>
-	<div>
-		<NuxtRouteAnnouncer />
-		<NuxtWelcome />
-	</div>
+	<NuxtLayout>
+		<NuxtPage />
+
+		<Toast />
+	</NuxtLayout>
 </template>
---
+++ apps/web/app/layouts/auth.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="flex min-h-screen items-center justify-center">
+		<div class="container max-w-md py-8">
+			<div class="mb-4 flex items-center justify-center">
+				<img :src="Images.TransparentLogo" alt="logo" class="max-w-20" />
+			</div>
+
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { Images } from "@repo/assets";
+</script>
