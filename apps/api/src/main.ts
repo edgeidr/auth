cccc8eb70c94566a777f5ec5f1da728d8e7fc334
edgeidr <edge.idr@gmail.com>
2025-12-11 00:40:12 +0800

    feat(api): initialize nestjs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import cookieParser from "cookie-parser";
+import { BadRequestException, ValidationPipe } from "@nestjs/common";
+import { ValidationError } from "class-validator";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3001);
+	const configService = app.get(ConfigService);
+	const host = configService.get<string>("API_HOST")!;
+	const port = configService.get<number>("API_PORT")!;
+	const corsAllowedOriginRaw = configService.get<string>("CORS_ALLOWED_ORIGIN", "");
+	const corsAllowedOrigin = corsAllowedOriginRaw.split(",").map((origin) => origin.trim());
+
+	app.use(cookieParser());
+	app.enableCors({
+		origin: corsAllowedOrigin,
+		credentials: true,
+	});
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+			exceptionFactory: (validationErrors: ValidationError[] = []) => {
+				return new BadRequestException(
+					validationErrors.map((error) => ({
+						field: error.property,
+						error: error.constraints ? Object.values(error.constraints) : "",
+					})),
+				);
+			},
+		}),
+	);
+
+	await app.listen(port, host);
 }
-bootstrap();
+
+void bootstrap();
