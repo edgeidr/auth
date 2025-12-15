cf6f9d070320825fd2be3052eb65419f92290d6d
edgeidr <edge.idr@gmail.com>
2025-12-15 23:00:14 +0800

    feat(web): add field-error component

---
+++ apps/web/app/components/shared/field-error.vue
@@ -0,0 +1,18 @@
+<template>
+	<div v-if="error?.length" class="mt-1">
+		<Message
+			v-for="(message, index) in error"
+			:key="index"
+			severity="error"
+			variant="simple"
+			size="small">
+			{{ message }}
+		</Message>
+	</div>
+</template>
+
+<script setup lang="ts">
+	const props = defineProps<{
+		error: string[] | null;
+	}>();
+</script>
