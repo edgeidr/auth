6d8510ee3439293b6bfa735a2c7328aa82cb294a
edgeidr <edge.idr@gmail.com>
2026-01-08 22:55:30 +0800

    refactor(web): remove Active Sessions and Recent Activity sections

--- apps/web/app/pages/account/security/index.vue
+++ apps/web/app/pages/account/security/index.vue
@@ -3,14 +3,10 @@
 		<EmailAddress />
 		<Password />
 		<SocialAccounts />
-		<ActiveSessions />
-		<RecentActivity />
 	</div>
 </template>
 
 <script setup lang="ts">
-	import ActiveSessions from "./_components/active-sessions.vue";
-	import RecentActivity from "./_components/recent-activity.vue";
 	import EmailAddress from "./_components/email-address.vue";
 	import Password from "./_components/password.vue";
 	import SocialAccounts from "./_components/social-accounts.vue";
